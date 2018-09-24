90e7d6b00a000785f99e66754b646f550a7ee463
Tomáš Milet <amillhouse@seznam.cz>
2018-09-24 17:24:43 +0200

    Added myRepositories.py

---
+++ myRepositories.py
@@ -0,0 +1,15 @@
+gits = [
+("git@github.com:dormon/SDL2CPP.git"          ,""             ,[]),
+("git@github.com:dormon/imguiDormon.git"      ,""             ,[]),
+("git@github.com:dormon/imguiOpenGLDormon.git",""             ,[]),
+("git@github.com:dormon/imguiSDL2Dormon.git"  ,""             ,[]),
+("git@github.com:dormon/imguiSDL2OpenGL.git"  ,""             ,[]),
+("git@github.com:dormon/geGL.git"             ,""             ,[]),
+("git@github.com:dormon/BasicCamera.git"      ,""             ,[]),
+("git@github.com:dormon/MealyMachine.git"     ,""             ,[]),
+("git@github.com:dormon/TxtUtils.git"         ,""             ,[]),
+("git@github.com:dormon/ArgumentViewer.git"   ,""             ,[]),
+("git@github.com:dormon/Vars.git"             ,""             ,[]),
+("git@github.com:dormon/checkRepos.git"       ,""             ,[]),
+("git@github.com:dormon/prj.git"              ,""             ,[]),
+        ]
