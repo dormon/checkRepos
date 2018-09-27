8ed2019302731e1c45d60eabf7865d47732a4f7b
Tomáš Milet <amillhouse@seznam.cz>
2018-09-27 22:19:11 +0200

    Added TessellationLevels to myCmakeApps.py

--- myCmakeApps.py
+++ myCmakeApps.py
@@ -1,5 +1,6 @@
 gits = [
-("git@github.com:dormon/prj.git"              ,""             ,[]),
-("git@github.com:dormon/MemoryBandwidth.git"  ,""             ,[]),
-("git@github.com:dormon/Shadows.git"          ,""             ,[]),
+("git@github.com:dormon/prj.git"               ,""             ,[]),
+("git@github.com:dormon/MemoryBandwidth.git"   ,""             ,[]),
+("git@github.com:dormon/Shadows.git"           ,""             ,[]),
+("git@github.com:dormon/TessellationLevels.git",""             ,[]),
         ]
