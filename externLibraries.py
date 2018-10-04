2afa06b551cc7d6ee4c9436cabb07a7010d33527
Tomáš Milet <amillhouse@seznam.cz>
2018-10-04 22:42:38 +0200

    Replaced assimp with my assimp

--- externLibraries.py
+++ externLibraries.py
@@ -1,5 +1,5 @@
 gits = [
 ("git@github.com:spurious/SDL-mirror.git"     ,"release-2.0.8",[]),
-("git@github.com:assimp/assimp.git"           ,""             ,["-DASSIMP_BUILD_SAMPLES=OFF","-DASSIMP_BUILD_ASSIMP_TOOLS=OFF","-DASSIMP_BUILD_TESTS=OFF"]),
+("git@github.com:dormon/assimp.git"           ,""             ,["-DASSIMP_BUILD_SAMPLES=OFF","-DASSIMP_BUILD_ASSIMP_TOOLS=OFF","-DASSIMP_BUILD_TESTS=OFF"]),
 ("git@github.com:g-truc/glm.git"              ,""             ,["-DGLM_TEST_ENABLE=OFF"]),
 ]
