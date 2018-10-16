aa79b28923f9a074f6dafe8153758686b0651ebe
Tomáš Milet <amillhouse@seznam.cz>
2018-10-16 09:28:53 +0200

    Added Empty3DProject

--- myCmakeApps.py
+++ myCmakeApps.py
@@ -3,4 +3,5 @@ gits = [
 ("git@github.com:dormon/MemoryBandwidth.git"   ,""             ,[]),
 ("git@github.com:dormon/Shadows.git"           ,""             ,[]),
 ("git@github.com:dormon/TessellationLevels.git",""             ,[]),
+("git@github.com:dormon/Empty3DProject.git"    ,""             ,[]),
         ]
